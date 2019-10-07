fae81476f4e3a30682f56bd0e07a66d19abe7ba5
Thomas Poiesz <thomas.poiesz@finaps.nl>
2019-10-07 17:03:48 +0200

    Remove unused imports

--- EventBus.IntegrationTests/InMemoryEventBusTests.cs
+++ EventBus.IntegrationTests/InMemoryEventBusTests.cs
@@ -1,16 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging.Abstractions;
-using Xunit;
-using Finaps.EventBus.IntegrationTests.Events;
-using System.Linq;
-using Finaps.EventBus.Core.Abstractions;
-using Finaps.EventBus.InMemory.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System.Threading;
-using System.Collections.Generic;
-using EventBus.IntegrationTests.Events;
-using System.Threading.Tasks;
-
 namespace Finaps.EventBus.IntegrationTests
 {
   public class InMemoryEventBusTests : BaseEventBusTests
--- EventBus.IntegrationTests/RabbitMQEventBusTests.cs
+++ EventBus.IntegrationTests/RabbitMQEventBusTests.cs
@@ -1,15 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using Finaps.EventBus.Core.Abstractions;
-using Finaps.EventBus.IntegrationTests.Events;
-using Finaps.EventBus.RabbitMQ;
-using Finaps.EventBus.RabbitMQ.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging.Abstractions;
-using Xunit;
-
 namespace Finaps.EventBus.IntegrationTests
 {
   public class RabbitMQEventBusTests : BaseEventBusTests
