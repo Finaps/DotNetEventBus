330ae71d31adb64957fd347b220a4baf4616c061
Simon Martyr <simon.martyr@finaps.nl>
2022-01-25 17:52:09 +0100

    functional test impicit using clean up

--- EventBus.IntegrationTests/Events/CheckConcurrencyEventHandler.cs
+++ EventBus.IntegrationTests/Events/CheckConcurrencyEventHandler.cs
@@ -1,20 +1,16 @@
-using System.Threading.Tasks;
 using Finaps.EventBus.Core.Abstractions;
-using System;
 
-namespace EventBus.IntegrationTests.Events
+namespace EventBus.IntegrationTests.Events;
+public class CheckConcurrencyEventHandler : IIntegrationEventHandler<CheckConcurrencyEvent>
 {
-  public class CheckConcurrencyEventHandler : IIntegrationEventHandler<CheckConcurrencyEvent>
-  {
-    private readonly IntegerIncrementer _integerIncrementer;
+  private readonly IntegerIncrementer _integerIncrementer;
 
-    public CheckConcurrencyEventHandler(IntegerIncrementer integerIncrementer)
-    {
-      _integerIncrementer = integerIncrementer;
-    }
-    public async Task Handle(CheckConcurrencyEvent @event)
-    {
-      await _integerIncrementer.Increment();
-    }
+  public CheckConcurrencyEventHandler(IntegerIncrementer integerIncrementer)
+  {
+    _integerIncrementer = integerIncrementer;
+  }
+  public async Task Handle(CheckConcurrencyEvent @event)
+  {
+    await _integerIncrementer.Increment();
   }
 }
--- EventBus.IntegrationTests/Events/EventPublisherEvent.cs
+++ EventBus.IntegrationTests/Events/EventPublisherEvent.cs
@@ -1,10 +1,7 @@
 
 using Finaps.EventBus.Core.Models;
-
-namespace Finaps.EventBus.IntegrationTests.Events
+namespace Finaps.EventBus.IntegrationTests.Events;
+public class EventPublisherEvent : IntegrationEvent
 {
-  public class EventPublisherEvent : IntegrationEvent
-  {
 
-  }
 }
