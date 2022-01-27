89106447d68762f3feda3b8435c7c1d749501b2b
Sander Meester <sander.meester@finaps.nl>
2022-01-27 13:31:24 +0100

    setup working with pooling

--- EventBus.Kafka/KafkaEventPublisher.cs
+++ EventBus.Kafka/KafkaEventPublisher.cs
@@ -2,6 +2,7 @@
 using Finaps.EventBus.Core.Abstractions;
 using Finaps.EventBus.Kafka.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ObjectPool;
 using System;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ namespace EventBus.Kafka
     // private bool _disposed;
     private readonly ILogger<KafkaEventPublisher> _logger;
     private KafkaOptions _options;
-    private IProducer<int, string> _producer;
+    private ObjectPool<IProducer<int, string>> _channelPool;
     internal KafkaEventPublisher(
       ILogger<KafkaEventPublisher> logger,
       KafkaOptions options
@@ -21,17 +22,22 @@ namespace EventBus.Kafka
     {
       _logger = logger;
       _options = options;
-      _producer = new ProducerBuilder<int, string>(
-        new ProducerConfig{ BootstrapServers = options.Brokers }
-      ).Build(); //Todo Get it from pool here
+      _channelPool = CreateChannelPool();
     }
+    
+
     public void Publish(string message, string eventName, string messageId)
     {
-      _producer.Produce("test", CreateMessage(message, eventName));
+      var channel = _channelPool.Get();
+      channel.Produce("test", CreateMessage(message, eventName));
+      _channelPool.Return(channel);
     }
     public Task PublishAsync(string message, string eventName, string messageId)
     {
-      return _producer.ProduceAsync("test", CreateMessage(message, eventName));
+      var channel = _channelPool.Get();
+      channel.ProduceAsync("test", CreateMessage(message, eventName));
+      _channelPool.Return(channel);
+      return Task.CompletedTask;
     }
     private Message<int, string> CreateMessage(string message, string eventName)
     {
@@ -39,6 +45,37 @@ namespace EventBus.Kafka
       headers.Add("eventName", Encoding.ASCII.GetBytes(eventName) );
       var key = new Random().Next(9999);
       return new Message<int, string> { Headers = headers, Key=key, Value = message };
+    }
+
+    private ObjectPool<IProducer<int, string>> CreateChannelPool()
+    {
+      var channelPoolPolicy = new ChannelPoolPolicy(_options.Brokers ??= "");
+      var channelPoolProvider = new DefaultObjectPoolProvider()
+      {
+        MaximumRetained = 10
+      };
+      return channelPoolProvider.Create(channelPoolPolicy);
+    }
+
+    private class ChannelPoolPolicy : PooledObjectPolicy<IProducer<int, string>>
+    {
+      private readonly ProducerConfig _producerConfig;
+
+      public ChannelPoolPolicy(string brokers)
+      {
+        this._producerConfig = new ProducerConfig{ BootstrapServers = brokers };
+      }
+
+      public override IProducer<int, string> Create()
+      {
+        var channel = new ProducerBuilder<int, string>(_producerConfig).Build();
+        return channel;
+      }
+
+      public override bool Return(IProducer<int, string> obj)
+      {
+        return obj != null;
+      }
     }
 
     public ValueTask DisposeAsync()
